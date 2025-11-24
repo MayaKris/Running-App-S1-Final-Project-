0444a07fb24b7ea0e11719f4bbe55e0032375891
Maya Krishnan <mayark527@gmail.com>
2025-11-24 10:56:27 -0600

    Initial Commit

---
+++ Running App (S1 Final Project)/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Running App (S1 Final Project)
+//
+//  Created by Maya Krishnan on 11/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Running App (S1 Final Project)/Running_App__S1_Final_Project_App.swift
@@ -0,0 +1,17 @@
+//
+//  Running_App__S1_Final_Project_App.swift
+//  Running App (S1 Final Project)
+//
+//  Created by Maya Krishnan on 11/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Running_App__S1_Final_Project_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
