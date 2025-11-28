9b2777c001ffe48c2f25b5a8c6cc0f5119f56d24
Maya Krishnan <mayark527@gmail.com>
2025-11-27 18:56:43 -0600

    Added welcome message and image

--- Running App (S1 Final Project)/ContentView.swift
+++ Running App (S1 Final Project)/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(" ")
+                .font(Font.custom("Party LET", size: 55))
+            Text("welcome")
+                .font(Font.custom("Party LET", size: 70))
+            Text("Runner!")
+                .font(Font.custom("Party LET", size: 100)).italic()
+            Image("TwoRunners").resizable().frame(width: 275, height: 250)
+            Spacer()
         }
-        .padding()
     }
 }
 
