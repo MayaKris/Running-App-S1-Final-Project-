890efff143f91051f3824234c6f4f3221328247e
Maya Krishnan <mayark527@gmail.com>
2025-11-27 18:55:40 -0600

    Added welcome message and image

---
+++ Running App (S1 Final Project)/Stopwatch.swift
@@ -0,0 +1,18 @@
+//
+//  Stopwatch.swift
+//  Running App (S1 Final Project)
+//
+//  Created by Maya Krishnan on 11/26/25.
+//
+
+import SwiftUI
+
+struct Stopwatch: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Stopwatch()
+}
