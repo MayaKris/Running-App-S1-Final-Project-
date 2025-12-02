54a052bf57aabf2e3b513c035783f6dcf2e55aa4
Maya Krishnan <mayark527@gmail.com>
2025-12-01 22:33:16 -0600

    Added NavigationStack{ } (and figured out why my NavigationLink wasn't taking me to the stopwatch view as intended)

--- Running App (S1 Final Project)/ContentView.swift
+++ Running App (S1 Final Project)/ContentView.swift
@@ -9,24 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack{
-            Color.blue.opacity(0.4).ignoresSafeArea() // changed background color to light blue
-            VStack {
-                Text(" ") // added space above title
-                    .font(Font.custom("Party LET", size: 40))
-                Text("welcome")
-                    .font(Font.custom("Party LET", size: 75))
-                Text("Runner!")
-                    .font(Font.custom("Party LET", size: 100)) // added titles with custom font and swize
-                Image("TwoRunners").resizable().frame(width: 300, height: 250) // added image of two runners (potential app icon?)
-                    .padding(10)
-                Spacer()
-                HStack{
-                    NavigationLink("time my run ⏱︎", destination: StopwatchView()) // navigation link to go to stopwatch view (time your run)
+        NavigationStack{
+            ZStack{
+                Color.blue.opacity(0.4).ignoresSafeArea() // changed background color to light blue
+                VStack {
+                    Text(" ") // added space above title
+                        .font(Font.custom("Party LET", size: 40))
+                    Text("welcome")
                         .font(Font.custom("Party LET", size: 75))
-                    
+                    Text("Runner!")
+                        .font(Font.custom("Party LET", size: 100)) // added titles with custom font and swize
+                    Image("TwoRunners").resizable().frame(width: 300, height: 250) // added image of two runners (potential app icon?)
+                        .padding(10)
+                    Spacer()
+                    HStack{
+                        NavigationLink("time my run ⏱︎", destination: StopwatchView()) // navigation link to go to stopwatch view (time your run)
+                            .font(Font.custom("Didot", size: 45))
+                        
+                    }
+                    Spacer()
                 }
-                Spacer()
             }
         }
     }
---
+++ Running App (S1 Final Project)/PaceView.swift
@@ -0,0 +1,18 @@
+//
+//  PaceView.swift
+//  Running App (S1 Final Project)
+//
+//  Created by Maya Krishnan on 12/1/25.
+//
+
+import SwiftUI
+
+struct PaceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PaceView()
+}
